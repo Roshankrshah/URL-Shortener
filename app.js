44a4a7669229e84d776cd146acfcf3f03147baa0
Roshan <shahroshan0720@gmail.com>
2023-07-04 14:16:38 +0530

    setup view template

--- app.js
+++ app.js
@@ -1,5 +1,19 @@
 const express = require('express');
+const shortId = require('shortid');
+const createHttpError = require('http-errors');
+const mongoose = require('mongoose');
+const path = require('path');
+
 const app = express();
+app.use(express.static(path.join(__dirname,'public')));
+app.use(express.json());
+app.use(express.urlencoded({extended:false}));
+
+app.set('view engine','ejs');
+
+app.get('/', async(req,res,next)=>{
+    res.render('index');
+})
 
 const PORT = 5000;
 
